b1054d3634fc8c077630252c44aecf090d6e238a
Echo413 <109029497+Echo413@users.noreply.github.com>
2025-08-25 14:02:40 +0300

    Update server.js

--- backend/backend/server.js
+++ backend/backend/server.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: "http://localhost:3000", 
+  origin: "https://skillswap-qmb5.onrender.com", 
   credentials: true
 }));
 
